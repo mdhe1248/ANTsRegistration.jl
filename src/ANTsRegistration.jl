be1fd62ed39716bec92c284cecb7297ea1786eea
Donghoon <mdhe1248@gmail.com>
2024-10-02 18:34:09 -0400

    bugfix: join -> joinpath

--- src/ANTsRegistration.jl
+++ src/ANTsRegistration.jl
@@ -342,7 +342,7 @@ end
 function register(fixed::AbstractArray, moving, pipeline::AbstractVector{<:Stage}; kwargs...)
     @info "`save_tform_file` is forcefully set to be false. Transform files are not saved on the disk."
     up = userpath()
-    outname = join(up, randstring(10))
+    outname = joinpath(up, randstring(10))
     if isa(kwargs, NamedTuple)
         kwargs = merge(kwargs, (save_tform_file = false,)) #Forcefully `set save_tform_file = false`
     else
